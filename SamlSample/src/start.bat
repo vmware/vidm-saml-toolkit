879af164ed329eb8decfa7bdc5074cecff3d2ee2
Sam Zhao <yisanz@vmwarae.com>
2017-01-16 13:19:25 +0800

    Code review #1

--- SamlSample/src/start.bat
+++ SamlSample/src/start.bat
@@ -4,10 +4,9 @@ echo  This can not be executed in src folder. Please execute it in target folder
 set curPath=%~dp0
 @echo Current Folder %cd%
 @ECHO Setting up classpath
-FOR %%F IN (%cd%\lib\*.jar) DO call :addcp %%F
+SET CLASSPATH=lib/*
 goto extlibe
-:addcp
-SET CLASSPATH=%CLASSPATH%;%1
+
 goto :eof
 :extlibe
 SET CLASSPATH 
@@ -15,5 +14,5 @@ SET CLASSPATH
 IF "%JAVA_HOME%" == "" (
     echo Please install JDK.
 ) ELSE (
-   "%JAVA_HOME%\BIN\java.exe" -classpath "%CLASSPATH%;SamlSample.jar" sample.MyServer
+   "%JAVA_HOME%\BIN\java.exe" -classpath "%CLASSPATH%;SamlSample.jar" com.vmware.eucenablement.saml.sample.MyServer
 )
